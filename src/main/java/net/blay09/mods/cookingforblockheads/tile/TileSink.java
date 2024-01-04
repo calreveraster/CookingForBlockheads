1cf7bb2414b5ac947d1bb536ac8bfd0c7606d64c
bombcar <github@bombcar.com>
2024-01-04 14:00:41 -0600

    update buildscript and dependencies (#41)
    * update buildscript and dependencies
    
    * spotless

--- src/main/java/net/blay09/mods/cookingforblockheads/tile/TileSink.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/tile/TileSink.java
@@ -14,7 +14,13 @@ import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
-import net.minecraftforge.fluids.*;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidHandler;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
